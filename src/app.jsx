84060c8e0b3d5d1a036c68a6575a868685028337
OBPSG <vbeasupergamer@gmail.com>
2022-03-17 11:01:28 -0500

    Added Moment Node library to display time stamps for each post

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import Moment from 'react-moment';
 import Post from './components/post';
 
 function App() {
     const initialPostArr = [
         {
             userName: "Some Rando",
-            body: "This is a test post!"
+            body: "This is a test post!",
+            time: "2022-03-16T17:30"
         },
         {
             userName: "OBP Super Gamer",
-            body: "Video Games are lots of fun!"
+            body: "Video Games are lots of fun!",
+            time: "2022-03-16T17:42"
         },
         {
             userName: "Leftover Sundriez Man",
-            body: "I like to build stuff and listen to electronic music!"
+            body: "I like to build stuff and listen to electronic music!",
+            time: "2022-03-16T18:12"
         }
     ];
 
@@ -27,7 +31,8 @@ function App() {
         console.log("Post button clicked!");
         const newPostList = [{
             userName: userName,
-            body: postText
+            body: postText,
+            
         }].concat(postList);
         setPostList(newPostList);
         setUserName("");
@@ -38,7 +43,7 @@ function App() {
         <div className="container">
             <h1>Latest Posts on Chirper:</h1>
             {postList.map((post) => {postKey += 1;
-                return <Post key={postKey} userName={post.userName} body={post.body}></Post>})}
+                return <Post key={postKey} userName={post.userName} body={post.body} time={post.time}></Post>})}
             <form className="form-group col-9 mt-4" onSubmit={handleSubmit}>
                 <label htmlFor="userName">User Name:</label>
                 <input value={userName} onChange={(event) => setUserName(event.target.value)} type="text" className="form-control w-50" id="userName" name="userName"></input>
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 function Post(props) {
     return(
         <div className="card col-9 mt-2">
             <h2 className="card-title">{props.userName}</h2>
             <p className="card-body">{props.body}</p>
+            <h6>Posted on <Moment>{props.time}</Moment></h6>
         </div>
     );
 }
