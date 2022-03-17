683ebd7ab3f10232960c3946af4fd1927be8b69b
OBPSG <vbeasupergamer@gmail.com>
2022-03-17 14:09:19 -0500

    Made some more cosmetic tweaks

--- src/app.jsx
+++ src/app.jsx
@@ -5,9 +5,9 @@ import Post from './components/post';
 function App() {
     const initialPostArr = [
         {
-            userName: "Some Rando",
-            body: "This is a test post!",
-            time: "2022-03-16T17:30"
+            userName: "Leftover Sundriez Man",
+            body: "I like to build stuff and listen to electronic music!",
+            time: "2022-03-16T18:12"
         },
         {
             userName: "OBP Super Gamer",
@@ -15,9 +15,9 @@ function App() {
             time: "2022-03-16T17:42"
         },
         {
-            userName: "Leftover Sundriez Man",
-            body: "I like to build stuff and listen to electronic music!",
-            time: "2022-03-16T18:12"
+            userName: "Some Rando",
+            body: "This is a test post!",
+            time: "2022-03-16T17:30"
         }
     ];
 
@@ -48,11 +48,11 @@ function App() {
                 postKey += 1;
                 return <Post key={postKey} userName={post.userName} body={post.body} time={post.time}></Post>
             })}
-            <form className="form-group col-md-9 mt-4" onSubmit={handleSubmit}>
+            <form className="form-group mt-4 col-md-9" onSubmit={handleSubmit}>
                 <label>Create New Post</label>
                 <input value={userName} onChange={(event) => setUserName(event.target.value)} type="text" className="form-control w-50" id="userName" name="userName" placeholder="User Name"></input>
-                <textarea value={postText} onChange={(event) => setPostText(event.target.value)} type="text" className="form-control" placeholder="Post text here..."></textarea>
-                <input type="submit" value="Post" className="btn mt-2 px-5 bg-success text-white"></input>
+                <textarea value={postText} onChange={(event) => setPostText(event.target.value)} type="text" className="form-control mt-2" placeholder="Post Text Here..."></textarea>
+                <input type="submit" value="Post" className="btn mt-2 px-5 bg-success text-white float-end"></input>
             </form>
         </div>
     );
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Moment from 'react-moment';
 
 function Post(props) {
-    return(
+    return (
         <div className="card col-md-9 mt-3 shadow border-primary">
             <h2 className="card-title mx-2">{props.userName}</h2>
             <div className="card-body">
                 <p>{props.body}</p>
             </div>
-            <h6 className="text-muted" style={{textAlign: "right"}}>Posted on <Moment>{props.time}</Moment></h6>
+            <h6 className="card-footer text-muted">Posted on <Moment>{props.time}</Moment></h6>
         </div>
     );
 }
